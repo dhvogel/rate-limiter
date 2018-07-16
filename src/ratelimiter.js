5cd6b9c1e70ab7f4d48bcfc0037b6f4ecec79ab1
dhvogel <dhvogel2468@gmail.com>
2018-07-15 20:48:41 -0700

    tests passing

--- src/ratelimiter.js
+++ src/ratelimiter.js
@@ -1,5 +1,70 @@
 'use strict';
 
-module.exports.canSpawn = function() {
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ spawns: [] })
+	.write();
+
+module.exports.canSpawn = function(limits, serviceName) {
+// limits is an array of objects, each object must have the fields:
+// 'maxSpawns' and 'seconds'
+
+	// make sure the required inputs exist, else return false
+	if (!limits || !serviceName) return false;
+
+	// if there are no limits on service spawning, return true
+	if (limits.length === 0) return true;
+
+	// UTC Timestamp of current time
+	const curTime = Date.now();
+
+	let previousSpawns, maxSpawns, seconds;
+	for (let i = 0; i < limits.length; i++) {
+		maxSpawns = limits[i].maxSpawns;
+		seconds = limits[i].seconds;
+
+		// if any limit object is malformed, return false
+		if (maxSpawns == null || seconds == null) return false;
+
+		// iterate through limit objects to see if requirements have been met for
+		// each
+
+		// get spawns for serviceName from db
+		previousSpawns = db.get('spawns')
+			.filter({ serviceName: serviceName })
+			.value();
+
+		// initialize spawn counter (count spawns that have occurred within the
+		// given spawn window)
+		// timePtr is the time when the spawn interval in question begins. We want
+		// to count how many spawns have occurred since the spawn interval began.
+		let counter = 0, timePtr = curTime - (seconds*1000);
+		previousSpawns.forEach((previousSpawn) => {
+			if (previousSpawn.timestamp >= (timePtr)) {
+				counter++;
+			}
+		});
+
+		// if we see more spawns than we allow, then return false
+		if (counter >= maxSpawns) return false;
+	}
+
+	// spawn available, simulating in db
+	db.get('spawns')
+		.push({ serviceName: serviceName, timestamp: Date.now() })
+		.write();
+
+	// return true, we just spawned a new service
 	return true;
+
+};
+
+// clear spawn DB in same context for testing purposes
+module.exports.clearDB = function() {
+	db.get('spawns')
+		.remove()
+		.write();
 };
--- test/unit/ratelimiter.unit.tests.js
+++ test/unit/ratelimiter.unit.tests.js
@@ -14,10 +14,90 @@ describe('Rate Limiter -- Unit Tests', function() {
 		one.should.equal(1);
 	});
 
-	describe('ratelimiter.canSpawn', function() {
-
-		it('should return true', () => {
-			(ratelimiter.canSpawn()).should.equal(true);
-		});
+	afterEach(function() {
+		ratelimiter.clearDB();
 	});
+
+	it('should return true if no limits', () => {
+		const limits = [];
+		const svcName = 'testSvc';
+
+		ratelimiter.canSpawn(limits, svcName).should.equal(true);
+
+	});
+
+	it('should return false if limits malformed', () => {
+		const limits = [
+			{
+				foo: 'bar',
+				baz: 'quux'
+			}
+		];
+		const svcName = 'testSvc';
+
+		ratelimiter.canSpawn(limits, svcName).should.equal(false);
+	});
+
+	it('should return false whenever maxSpawns is zero', () => {
+		const limits = [
+			{
+				maxSpawns: 0,
+				seconds: 100
+			}
+		];
+		const svcName = 'testSvc';
+		ratelimiter.canSpawn(limits, svcName).should.equal(false);
+	});
+
+	it('should return true if limits allow', () => {
+		const limits = [
+			{
+				maxSpawns: 1,
+				seconds: 1
+			}
+		];
+		const svcName = 'testSvc';
+		ratelimiter.canSpawn(limits, svcName).should.equal(true);
+	});
+
+	it('should return true until limit has been reached', () => {
+		const limits = [
+			{
+				maxSpawns: 100,
+				seconds: 100
+			},
+			{
+				maxSpawns: 2,
+				seconds: 20
+			}
+		];
+
+		const svcName = 'testSvc';
+		ratelimiter.canSpawn(limits, svcName).should.equal(true);
+		ratelimiter.canSpawn(limits, svcName).should.equal(true);
+		ratelimiter.canSpawn(limits, svcName).should.equal(false);
+	});
+
+	it('should return true until limit has been reached', (done) => {
+		const limits = [
+			{
+				maxSpawns: 4,
+				seconds: 1
+			},
+
+		];
+
+		const svcName = 'testSvc';
+
+		for (let i=0; i<4; i++) {
+			ratelimiter.canSpawn(limits, svcName).should.equal(true);
+		}
+
+		setTimeout(function() {
+			for (let i=0; i<4; i++) {
+				ratelimiter.canSpawn(limits, svcName).should.equal(true);
+			}
+			done();
+		}, 2000);
+	}).timeout(5000);
 });
